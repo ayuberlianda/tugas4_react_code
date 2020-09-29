5f124c100bd3319f6cfc7e67a991079b0c63e83f
ayuberlianda <71421525+ayuberlianda@users.noreply.github.com>
2020-09-30 00:22:42 +0700

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import './App.css';
+import MenuMakanan from './Page/MenuMakanan';
+
+function App() {
+  return (
+    <div>
+    <center>
+    <MenuMakanan/>
+    </center>
+    </div>
+  );
+}
+
+export default App;
